1f4f21513974f9587cf7ce33f398c76de17161b5
cpandares <>
2022-07-14 19:11:09 -0400

    project works

---
+++ components/Layout.jsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+const Layout = ({ children }) => {
+    return (
+        <div className='layout'>
+            {/* <Head>
+                <titl>Eccomerce</titl>
+            </Head> */}
+
+            <header>
+                <Navbar />
+
+                
+            </header>
+            <main className='main-container'>
+                { children }
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    );
+};
+
+export default Layout;
